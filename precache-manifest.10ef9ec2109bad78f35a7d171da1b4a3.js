6143cc8a4de1a87570bca4d0dfb3dcb78517ee31
roy <roy@syndrome.co.il>
2019-04-03 00:42:04 +0400

    Updates

---
+++ precache-manifest.10ef9ec2109bad78f35a7d171da1b4a3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4bcec4721da46c58f6517d18fba69e0",
+    "url": "/barbarosa/static/media/feature-3.a4bcec47.svg"
+  },
+  {
+    "revision": "054398a0f60cef1f9c1d",
+    "url": "/barbarosa/static/js/main.14552e8e.chunk.js"
+  },
+  {
+    "revision": "976f8ff118e16f9f2f3c4d83f7ca04ea",
+    "url": "/barbarosa/static/media/mx.976f8ff1.png"
+  },
+  {
+    "revision": "ea08f70ae7558a3a9b66cd9b78a898c4",
+    "url": "/barbarosa/static/media/sip.ea08f70a.svg"
+  },
+  {
+    "revision": "dfd66a16cf8c546a2394593f4b3fc21c",
+    "url": "/barbarosa/static/media/mobile.dfd66a16.svg"
+  },
+  {
+    "revision": "5a142b4a199fd5e6b314d79dbd87822d",
+    "url": "/barbarosa/static/media/feature-2.5a142b4a.svg"
+  },
+  {
+    "revision": "80c89b122473849df05dceabe120ea38",
+    "url": "/barbarosa/static/media/feature-4.80c89b12.svg"
+  },
+  {
+    "revision": "032edb9909770a12d4825889608b1dbb",
+    "url": "/barbarosa/static/media/feature-8.032edb99.svg"
+  },
+  {
+    "revision": "842071405144d56adae83cf14efd6250",
+    "url": "/barbarosa/static/media/feature-1.84207140.svg"
+  },
+  {
+    "revision": "6ce132a46cc81fa48f9e",
+    "url": "/barbarosa/static/js/runtime~main.e59d00bf.js"
+  },
+  {
+    "revision": "1f32d817b9480ec11e33a94e7cdc5ab4",
+    "url": "/barbarosa/static/media/feature-5.1f32d817.svg"
+  },
+  {
+    "revision": "1214937256d1944b5540af48095b380a",
+    "url": "/barbarosa/static/media/feature-6.12149372.svg"
+  },
+  {
+    "revision": "935e36dbe6bb76cef58ee3789488839d",
+    "url": "/barbarosa/static/media/feature-7.935e36db.svg"
+  },
+  {
+    "revision": "60e28aaadcf8114c1769141a65d703ca",
+    "url": "/barbarosa/static/media/feature-9.60e28aaa.svg"
+  },
+  {
+    "revision": "8783be4b964b6a0f48fc2bffba0cc930",
+    "url": "/barbarosa/static/media/sidebar-decoration.8783be4b.png"
+  },
+  {
+    "revision": "2acb85322760b57b6cecf661d3634819",
+    "url": "/barbarosa/static/media/welcome-background.2acb8532.png"
+  },
+  {
+    "revision": "a2d8b117bf97cd1b4e55",
+    "url": "/barbarosa/static/js/2.c783c4ad.chunk.js"
+  },
+  {
+    "revision": "b8e7a30ff849a49cc31c5298f21f3a41",
+    "url": "/barbarosa/index.html"
+  }
+];
